75b0c1c0f9aa6978611f36822da66b496374aed3
pyshkovni <guess.the.tutor@gmail.com>
2025-04-09 01:23:18 +0300

    Описал шаблон теста с фикстурой на pytest

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,22 +1,10 @@
 import pytest
-from aiogram import Router
-from aiogram.types import Message
-from handlers.handlers import command_start_handler, command_help_handler
-from unittest.mock import AsyncMock
 
-
-@pytest.fixture
-def mock_router():
-    """Mock-роутер"""
-    router = Router()
-    router.message.register(command_start_handler)
-    router.message.register(command_help_handler)
-    return router
-
+# Фикстуры в pytest позволяют выносить в отдельные функции типовые действия
+# например: настройка тестового окружения, создание тестовых данных, выполнение завершающие действия
+# https://habr.com/ru/articles/731296/
 
 @pytest.fixture
-def mock_message():
-    """Mock-сообщение"""
-    mock_msg = AsyncMock(spec=Message)
-    mock_msg.answer = AsyncMock()
-    return mock_msg
+def mock_():
+    """Mock"""
+    pass
